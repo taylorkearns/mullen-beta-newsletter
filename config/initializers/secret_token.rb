5ac6bd703f420d32019ade6dc8b28f4e50c60a44
Taylor Kearns <tkearns@gmail.com>
2011-12-24 16:31:47 -0500

    Initial commit. Column and container layout begun.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaNewsletter::Application.config.secret_token = 'ac11e36588b8909d2a52005f600ba8de3357d3d4f84bd4bd5fb4d208345b07acba31715aed9374898d7d7e8f12b75cd8e229a982dae96c4b8bbb265829a948c3'
